c5538088e155b694441597db2f82d925ec65c8c2
Jean-Noel.CANCHES <jean-noel.canches@berger-levrault.com>
2020-09-15 13:47:06 +0200

    Bumped version to 1.0.3 Updated some help links

--- blindcontroller/blindcontroller.js
+++ blindcontroller/blindcontroller.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2015, 2016 Alisdair Smyth
+ * Copyright 2020, Jean-Noël Canches
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
