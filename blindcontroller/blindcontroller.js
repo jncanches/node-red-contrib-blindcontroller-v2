c5f01684499b59838e3acdb70815527c445c7dcb
Jean-Noel.CANCHES <jean-noel.canches@berger-levrault.com>
2020-09-16 16:31:54 +0200

    Bumped version to 1.0.5 Sun actions now send a message for each blind that must be changed.

--- blindcontroller/blindcontroller.js
+++ blindcontroller/blindcontroller.js
@@ -767,7 +767,9 @@ module.exports = function(RED) {
    */
   function runCalc(node, send, msg, blinds, sunPosition, weather) {
     var i;
+    var resultMsgs = [];
     for (i in blinds) {
+      node.error("runCalc for " + i);
       var previousBlindPosition = blinds[i].blindPosition;
       var previousSunInWindow = blinds[i].sunInWindow;
       var previousBlindPositionReasonCode = blinds[i].blindPositionReasonCode;
@@ -776,24 +778,31 @@ module.exports = function(RED) {
 
       blinds[i].logicalBlindPosition = blinds[i].blindPosition;
       blinds[i].blindPosition = blinds[i].opposite
-        ? 100 - blinds[i].blindPosition
-        : blinds[i].blindPosition;
+          ? 100 - blinds[i].blindPosition
+          : blinds[i].blindPosition;
 
       if (
-        blinds[i].blindPosition != previousBlindPosition ||
-        blinds[i].sunInWindow != previousSunInWindow ||
-        blinds[i].blindPositionReasonCode != previousBlindPositionReasonCode
+          blinds[i].blindPosition != previousBlindPosition ||
+          blinds[i].sunInWindow != previousSunInWindow ||
+          blinds[i].blindPositionReasonCode != previousBlindPositionReasonCode
       ) {
-        msg.payload = blinds[i];
-        msg.data = {
+        //node.error("runCalc has changed for " + i);
+        var resultMsg = RED.util.cloneMessage(msg);
+
+        resultMsg.payload = blinds[i];
+        resultMsg.data = {
           channel: blinds[i].channel,
           altitude: sunPosition.altitude,
           azimuth: sunPosition.azimuth,
           blindPosition: blinds[i].blindPosition
         };
-        msg.topic = "blind";
-        send(msg);
+        resultMsg.topic = "blind";
+        resultMsgs.push(resultMsg);
       }
+    }
+    if (resultMsgs.length > 0) {
+      //node.error("runCalc results length " + resultMsgs.length);
+      node.send([resultMsgs]);
     }
   }
 
